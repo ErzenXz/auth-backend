b0f79013e22ae63052d5aca90b856e30f140bdda
Erzen Krasniqi <48888391+ErzenXz@users.noreply.github.com>
2024-12-09 01:30:32 +0100

    feat: add Infisical run script and enhance token retrieval in script.sh (#27)

--- script.sh
+++ script.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=$INFISICAL_MACHINE_CLIENT_ID --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET --plain --silent)
+echo "Starting the script"
+echo "USING INFISICAL_MACHINE_CLIENT_ID: $INFISICAL_MACHINE_CLIENT_ID"
+echo "USING INFISICAL_MACHINE_CLIENT_SECRET: $INFISICAL_MACHINE_CLIENT_SECRET"
+echo "USING PROJECT_ID: $PROJECT_ID"
+export INFISICAL_TOKEN=$(infisical login --domain=https://eu.infisical.com/api --method=universal-auth \
+  --client-id=$INFISICAL_MACHINE_CLIENT_ID \
+  --client-secret=$INFISICAL_MACHINE_CLIENT_SECRET \
+  --plain --silent)
 exec infisical run --projectId $PROJECT_ID -- yarn start:prod
